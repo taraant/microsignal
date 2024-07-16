9b52c4c4c0c5cdc2758c8e8a583f9221ab0f8a44
Anton Tarasov <ant0@mail.ru>
2024-07-16 10:26:50 +0300

    Increase version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='microsignal',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     description='A lightweight library for handling custom signals with synchronous and asynchronous callbacks in Python.',
     long_description=open('README.md').read(),
